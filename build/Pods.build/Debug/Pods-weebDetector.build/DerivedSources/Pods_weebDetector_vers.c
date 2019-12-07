5c23ef41b6dc2bdb62daea1ed9dcabcc7c9ee8df
Yaroster <39062152+Yaroster@users.noreply.github.com>
2019-12-07 11:59:19 +0400

    Uploading build files

---
+++ build/Pods.build/Debug/Pods-weebDetector.build/DerivedSources/Pods_weebDetector_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_weebanalyserVersionString[];
+ extern const double Pods_weebanalyserVersionNumber;
+
+ const unsigned char Pods_weebanalyserVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_weebanalyser  PROJECT:Pods-1" "\n";
+ const double Pods_weebanalyserVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Pods.build/Debug/SwiftyXMLParser.build/Objects-normal/x86_64/SwiftyXMLParser-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/yaroster/Documents/weebanalyser-mac-master/Pods/Target\ Support\ Files/SwiftyXMLParser/SwiftyXMLParser-dummy.m \
+  /Users/yaroster/Documents/weebanalyser-mac-master/build/Pods.build/Debug/SwiftyXMLParser.build/module.modulemap \
+  /Users/yaroster/Documents/weebanalyser-mac-master/Pods/Target\ Support\ Files/SwiftyXMLParser/SwiftyXMLParser-prefix.pch
