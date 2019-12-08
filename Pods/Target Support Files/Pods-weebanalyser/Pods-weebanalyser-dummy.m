62164f3f24f0e89bf6022187b151c90723fd6349
Yaroster <39062152+Yaroster@users.noreply.github.com>
2019-12-08 22:03:04 +0400

    Adding final weebanalyser files 2

---
+++ Pods/Target Support Files/Pods-weebanalyser/Pods-weebanalyser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weebanalyser : NSObject
+@end
+@implementation PodsDummy_Pods_weebanalyser
+@end
---
+++ Pods/Target Support Files/Pods-weebanalyserTests/Pods-weebanalyserTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weebanalyserTests : NSObject
+@end
+@implementation PodsDummy_Pods_weebanalyserTests
+@end
