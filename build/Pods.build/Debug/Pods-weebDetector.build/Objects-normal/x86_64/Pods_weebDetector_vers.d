b12334e5147e67358b9f61c04263e92364242b6d
Yaroster <39062152+Yaroster@users.noreply.github.com>
2019-12-08 22:07:51 +0400

    Adding final weebanalyser files

---
+++ build/Pods.build/Debug/Pods-weebDetector.build/Objects-normal/x86_64/Pods_weebDetector_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/yaroster/Documents/weebanalyser-mac-master/build/Pods.build/Debug/Pods-weebanalyser.build/DerivedSources/Pods_weebanalyser_vers.c
---
+++ build/Pods.build/Debug/SwiftyXMLParser.build/Objects-normal/x86_64/SwiftyXMLParser_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/yaroster/Documents/weebanalyser-mac-master/build/Pods.build/Debug/SwiftyXMLParser.build/DerivedSources/SwiftyXMLParser_vers.c
